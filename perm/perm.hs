684738fb0cf14f67970fc843fead7924f06f2efa
Avi Itskovich <avi.itskovich@gmail.com>
2012-11-06 14:20:10 -0500

    Add PERM

--- perm/perm.hs
+++ perm/perm.hs
@@ -1,3 +1,19 @@
 perms :: [a] -> [[a]]
-perms [] = []
-perms x:xs = x +++  perms xs
+perms [] = [[]]
+perms (x:xs) = concat (map (concatall x) (perms xs))
+
+concatall :: a -> [a] -> [[a]]
+concatall x [] = [[x]]
+concatall x (y:ys) = [x:(y:ys)] ++ map (y:) (concatall x ys)
+
+-- Unused for Question --
+
+subset :: Int -> [a] -> [[a]]
+subset n [] = []
+subset 1 xs = map (\x -> [x]) xs
+subset n (x:xs) = (map (x:) $ subset (n - 1) xs) ++ rest
+    where
+        rest = if length xs >= n then subset n xs else []
+
+subperms :: Int -> [a] -> [[a]]
+subperms n xs = concat $ map perms $ subset n xs
