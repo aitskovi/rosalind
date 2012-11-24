6bac34ad12d319e62269c65152590f50a93a4c4d
Avi Itskovich <avi.itskovich@gmail.com>
2012-11-23 23:02:52 -0500

    Cleaner version of SUBS

--- subs/subs.hs
+++ subs/subs.hs
@@ -1,26 +1,13 @@
 -- Return the starting point of the locations of the substring in the
 -- original string.
 
+import Data.List
+
 subs :: String -> String -> [Int]
 subs xs "" = []
 subs "" ys = []
-subs xs ys = reverse $ subsHelper indexed ys []
-    where
-        indexed = zip [1..(length xs)] xs
-
-subsHelper :: [(Int, Char)] -> String -> [(Int, String)] -> [Int]
-subsHelper [] ys zs = map fst $ filter (\(index, remainder) -> (length remainder) == 0) zs
-subsHelper ((x1, x2):xs) (y:ys) zs = subsHelper xs (y:ys) matches
-    where
-        updated = updatePatterns x2 zs
-        matches = if y == x2 then (x1, ys):updated else updated
-
-updatePatterns :: Char -> [(Int, String)] -> [(Int, String)]
-updatePatterns c [] = []
-updatePatterns c ((index, []):ps) = (index, []):(updatePatterns c ps)
-updatePatterns c ((index, (r:rs)):ps) = if r == c then (index, rs):rest else rest
-    where
-        rest = updatePatterns c ps
+subs (x:xs) ys = if isPrefixOf ys (x:xs) then 1:rest else rest
+    where rest = map (+1) $ subs xs ys
 
 run :: [String] -> [Int]
 run (x1:x2:[]) = subs x1 x2
