95e00f092ffa1ba8b9a0055f03658a36fcb0c35a
Avi Itskovich <avi.itskovich@gmail.com>
2012-12-08 12:41:08 -0500

    Refactor GC to use interactFasta

--- gc/gc.hs
+++ gc/gc.hs
@@ -1,12 +1,7 @@
-import System.IO
+import Utils.Bioinformatics
 
-data Fasta = Fasta String String
-           | Empty
-
-
-main = interact f
+main = interactFasta $ print . gc
      where
-         f = print . gc . parse . lines
          print x = fname x ++ "\n" ++ (show $ percent x) ++ "\n"
 
 gc :: [Fasta] -> Fasta
@@ -19,16 +14,6 @@ percent (Fasta n d) =  gcCount / total * 100
         gcCount = fromIntegral $ length $ filter (\x -> (x == 'C') || (x == 'G')) d
         total = fromIntegral $ length d
 
--- Parse a FASTA data file
-parse :: [String] -> [Fasta]
-parse xs = foldl parseLine [] xs
-
--- Parse a FASTA data file line
-parseLine :: [Fasta] -> String -> [Fasta]
-parseLine xs [] = xs
-parseLine [] ys = if isName ys then [(parseName ys)] else []
-parseLine ((Fasta n d):xs) ys = if isName ys then (parseName ys):((Fasta n d):xs)
-                                            else (Fasta n (d ++ ys)):xs
 -- Generate a string from a Fasta DataType
 stringify :: Fasta -> String
 stringify (Fasta name value) = name ++ "\n" ++ value
@@ -37,11 +22,3 @@ fname :: Fasta -> String
 fname Empty = "Empty"
 fname (Fasta n d) = n
 
--- Verify that a string represents a FASTA Name
-isName :: String -> Bool
-isName [] = False
-isName (x:xs) = (x == '>')
-
--- Parse a FASTA Name
-parseName :: String -> Fasta 
-parseName (x:xs) = Fasta xs ""
