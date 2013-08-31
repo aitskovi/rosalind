e96183f34543e0bf5a5b1791718108c2f9de6675
Avi Itskovich <avi.itskovich@gmail.com>
2013-08-30 22:40:30 -0400

    Add PRTM solution

---
+++ prtm/prtm.hs
@@ -0,0 +1,28 @@
+main = interact $ show . weight
+
+-- weight :: (Fractional a) => String -> a
+weight xs = sum $ map mass xs
+
+-- Monoisotopic mass table
+mass :: (Fractional a) => Char -> a
+mass 'A' = 71.03711
+mass 'C' = 103.00919
+mass 'D' = 115.02694
+mass 'E' = 129.04259
+mass 'F' = 147.06841
+mass 'G' = 57.02146
+mass 'H' = 137.05891
+mass 'I' = 113.08406
+mass 'K' = 128.09496
+mass 'L' = 113.08406
+mass 'M' = 131.04049
+mass 'N' = 114.04293
+mass 'P' = 97.05276
+mass 'Q' = 128.05858
+mass 'R' = 156.10111
+mass 'S' = 87.03203
+mass 'T' = 101.04768
+mass 'V' = 99.06841
+mass 'W' = 186.07931
+mass 'Y' = 163.06333 
+mass _   = 0
